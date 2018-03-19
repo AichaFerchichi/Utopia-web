c327534980deeb2ebd18d4ec94760de625b45c0b
Smiriimen <36234918+Smiriimen@users.noreply.github.com>
2018-03-19 17:36:28 +0000

    pro

--- projetSymfonyPIDEV/src/UserBundle/Controller/DefaultController.php
+++ projetSymfonyPIDEV/src/UserBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
