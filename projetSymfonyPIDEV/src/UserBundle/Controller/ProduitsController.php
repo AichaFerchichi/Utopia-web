091493b9f85ecf2c4e9ea17321c2bccccad2d299
molkaouaja <36240334+molkaouaja@users.noreply.github.com>
2018-03-24 17:26:44 +0100

    MolkaFrontBundle

--- projetSymfonyPIDEV/src/UserBundle/Controller/ProduitsController.php
+++ projetSymfonyPIDEV/src/UserBundle/Controller/ProduitsController.php
@@ -37,16 +37,16 @@ class ProduitsController extends Controller
 
 
         //rechercher
-        $voiture2= new Produits();
+        /*$voiture2= new Produits();
         $form2=$this->createForm(RechercherProduits::class,$voiture2);
         if ($form2->handleRequest($request)->isValid())
         {
             $em=$this->getDoctrine()->getManager();
             $marks=$em->getRepository('UserBundle:Produits')->findBy(array('categorie'=>$voiture2->getCategorie()));
-        }
+        }*/
 
         return $this->render('BackBundle:Default:gestionProduits.html.twig', array(
-            'm'=>$marks , 'f'=>$form->createView() , 'f2'=>$form2->createView()
+            'm'=>$marks , 'f'=>$form->createView()
         ));
 
 
@@ -60,7 +60,7 @@ class ProduitsController extends Controller
         return $this->redirectToRoute('gestionProduits');
     }
 
-    function modifierAction($id,Request $request){
+    public function modifierAction($id,Request $request){
 
         $em=$this->getDoctrine()->getManager() ;
         $mark=$em->getRepository('UserBundle:Produits')->find($id) ;
@@ -81,5 +81,127 @@ class ProduitsController extends Controller
             return $this->redirectToRoute('gestionProduits') ;
         }        return $this->render('BackBundle:Default:modifierProduits.html.twig',array( 'e'=> $name , 'f'=>$form->createView())) ;
     }
+
+    public function rechercherAction(Request $request){
+        $em=$this->getDoctrine()->getManager();
+        $motcle=$request->get('motcle');
+        $repository=$em->getRepository('UserBundle:Produits');
+        $query=$em->createQueryBuilder()
+            ->select('p')->from('UserBundle:Produits','p')
+            ->where('p.categorie like :categorie')
+            ->setParameter('categorie','%'.$motcle.'%')
+            ->orderBy('p.categorie','ASC')
+            ->getQuery();
+
+        $produits=$query->getResult();
+//ajout
+        $joueur= new Produits();
+
+
+        $form=$this->createForm(ProduitsType::class,$joueur);
+        if ($form->handleRequest($request)->isValid())
+        {/**
+         * @var UploadedFile $file
+         */
+            $file=$joueur->getImage() ;
+            $fileName = md5(uniqid()).'.'.$file->guessExtension();
+            $file->move($this->getParameter('image_directory'),$fileName) ;
+            $joueur->setImage($fileName) ;
+            $em->persist($joueur) ;
+            $em->flush() ;
+            return $this->redirectToRoute('gestionProduits') ;
+
+        }
+
+        return $this->render('BackBundle:Default:gestionProduits.html.twig',array('m'=>$produits,'f'=>$form->createView()));
+    }
+
+    public function affichageProduitsAction(){
+        $em=$this->getDoctrine()->getManager();
+        $marks=$em->getRepository('UserBundle:Produits')->findAll();
+        return $this->render('FrontBundle:Default:Boutique.html.twig', array(
+            'm'=>$marks
+        ));
+    }
+
+    public function singleAction($id){
+        $em=$this->getDoctrine()->getManager();
+        $marks=$em->getRepository('UserBundle:Produits')->find($id);
+        $voitures=$em->getRepository('UserBundle:Promotions')->findOneBy(array('idProduit'=>$id)) ;
+        $mot='vêtements';
+        $query=$em->createQueryBuilder()
+            ->select('p')->from('UserBundle:Produits','p')
+            ->where('p.categorie like :categorie')
+            ->setParameter('categorie','%'.$mot.'%')
+            ->setMaxResults(4)
+            ->getQuery();
+
+        $some=$query->getResult();
+
+
+        return $this->render('FrontBundle:Default:single.html.twig', array(
+            'i'=>$marks , 'e'=>$voitures , 'some'=>$some
+        ));
+    }
+
+    public function single2Action($id){
+        $em=$this->getDoctrine()->getManager();
+        $marks=$em->getRepository('UserBundle:Produits')->find($id);
+        $voitures=$em->getRepository('UserBundle:Promotions')->findOneBy(array('idProduit'=>$id)) ;
+        $mot='jouets';
+        $query=$em->createQueryBuilder()
+            ->select('p')->from('UserBundle:Produits','p')
+            ->where('p.categorie like :categorie')
+            ->setParameter('categorie','%'.$mot.'%')
+            ->setMaxResults(4)
+            ->getQuery();
+
+        $some=$query->getResult();
+        return $this->render('FrontBundle:Default:single2.html.twig', array(
+            'i'=>$marks , 'e'=>$voitures , 'some'=>$some
+        ));
+    }
+
+    public function single3Action($id){
+        $em=$this->getDoctrine()->getManager();
+        $marks=$em->getRepository('UserBundle:Produits')->find($id);
+        $voitures=$em->getRepository('UserBundle:Promotions')->findOneBy(array('idProduit'=>$id)) ;
+        $mot='fournitures';
+        $query=$em->createQueryBuilder()
+            ->select('p')->from('UserBundle:Produits','p')
+            ->where('p.categorie like :categorie')
+            ->setParameter('categorie','%'.$mot.'%')
+            ->setMaxResults(4)
+            ->getQuery();
+
+        $some=$query->getResult();
+        return $this->render('FrontBundle:Default:single3.html.twig', array(
+            'i'=>$marks , 'e'=>$voitures , 'some'=>$some
+        ));
+    }
+
+    public function nosJouetsAction(){
+        $em=$this->getDoctrine()->getManager();
+        $marks=$em->getRepository('UserBundle:Produits')->findBy(array('categorie'=>'jouets'));
+        return $this->render('FrontBundle:Default:nosJouets.html.twig', array(
+            'm'=>$marks
+        ));
+    }
+    public function nosVetementsAction(){
+        $em=$this->getDoctrine()->getManager();
+        $marks=$em->getRepository('UserBundle:Produits')->findBy(array('categorie'=>'vêtements'));
+        return $this->render('FrontBundle:Default:nosVetements.html.twig', array(
+            'm'=>$marks
+        ));
+    }
+
+    public function nosFournituresAction(){
+        $em=$this->getDoctrine()->getManager();
+        $marks=$em->getRepository('UserBundle:Produits')->findBy(array('categorie'=>'fournitures'));
+        return $this->render('FrontBundle:Default:nosFournitures.html.twig', array(
+            'm'=>$marks
+        ));
+    }
+
 
 }
